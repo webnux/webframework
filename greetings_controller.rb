7e61eca6493d07ec9cc96e823b63379f2e69d692
DIG <kiru42@gmail.com>
2017-12-09 22:19:00 +0100

    add more controllers

---
+++ greetings_controller.rb
@@ -0,0 +1,11 @@
+class GreetingsController
+  def hello
+    status, body = Renderer.new("hello.html").render
+    [status, {}, [body]]
+  end
+  
+  def hola
+    status, body = Renderer.new("hola.html").render
+    [status, {}, [body]]
+  end
+end
